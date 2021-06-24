2c1d1939bad27bd4e80c11e1f4f2436e14adb480
geraldsanga <gerrysanga33@protonmail.ch>
2021-06-24 16:14:58 +0300

    Searching for the search string pattern in post descriptions

--- views/search_result.php
+++ views/search_result.php
@@ -40,7 +40,7 @@ header("location: register.php");
                 <?php 
                  $search_string = $_POST["search_string"];
                   $mysqli = new mysqli("localhost", "root", "Root*123", "social") or die(mysqli_error($mysqli)); 
-                  $result = $mysqli->query("SELECT * FROM Post WHERE title LIKE '%$search_string%' ORDER BY id DESC") or die($mysqli->error);
+                  $result = $mysqli->query("SELECT * FROM Post WHERE title LIKE '%$search_string%' OR description LIKE '%$search_string%' ORDER BY id DESC") or die($mysqli->error);
                   $num_rows = mysqli_num_rows($result);
                   if($num_rows > 0): 
                 ?>

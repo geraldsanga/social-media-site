b71e9f607e966809194fc2960bd238f4cd0aa998
geraldsanga <gerrysanga33@protonmail.ch>
2021-06-25 16:21:50 +0300

    The poke link has to be active when we are on that page

--- views/pokes.php
+++ views/pokes.php
@@ -29,9 +29,9 @@ if(!isset($_SESSION['user_logged_in']))
                 aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="../index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" aria-current="page" href="../index.php">Home</a></li>
                     <li class="nav-item"><a class="nav-link" aria-current="page" href="create_post.php">Post</a>
-                    <li class="nav-item"><a class="nav-link" href="pokes.php">Pokes</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="pokes.php">Pokes</a></li>
                     </li>
                     <li class="nav-item"><a class="nav-link" aria-current="page"
                             href="user_account.php">Account</a></li>

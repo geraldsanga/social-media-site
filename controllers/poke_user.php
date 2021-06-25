3c42b94ba6d40c32a7790e4edf0add55ff9c36ce
geraldsanga <gerrysanga33@protonmail.ch>
2021-06-25 17:23:10 +0300

    Users can only poke other users once

--- controllers/poke_user.php
+++ controllers/poke_user.php
@@ -6,9 +6,13 @@
     if(isset($_POST["poke_user"])){
         $pokee_user = $_POST["user_id"];
         $poking_user = $_SESSION["user_id"];
-        echo "pokee_id: $pokee_user" . "<br>";
-        echo "poker: $poking_user" . "<br>";
-        $mysqli->query("INSERT INTO UserPoke (pokee_id, poker_id) VALUES ('$pokee_user', '$poking_user')") or die($mysqli->error);
-        $_SESSION["poking_success"] = "Poked :)";
-        header("location: ../views/others_user_account.php?user_id=$pokee_user");
+        $results = $mysqli->query("SELECT pokee_id, poker_id from UserPoke WHERE pokee_id = $pokee_user && poker_id = $poking_user");
+        if(mysqli_num_rows($results) > 0){ // if there is a poke already
+            $_SESSION["poking_success"] = "Poked :)";
+            header("location: ../views/others_user_account.php?user_id=$pokee_user");
+        }else{ // if the user hasn't poked this user yet
+            $mysqli->query("INSERT INTO UserPoke (pokee_id, poker_id) VALUES ('$pokee_user', '$poking_user')") or die($mysqli->error);
+            $_SESSION["poking_success"] = "Poked :)";
+            header("location: ../views/others_user_account.php?user_id=$pokee_user");
+        }
     }

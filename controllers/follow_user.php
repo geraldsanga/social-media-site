f61b648d98f668f56d453285110226fb0854be2c
geraldsanga <gerrysanga33@protonmail.ch>
2021-06-25 16:20:45 +0300

    proper comments for follow user and created rendering pokes per user

--- controllers/follow_user.php
+++ controllers/follow_user.php
@@ -9,7 +9,7 @@
         $follow_results = $mysqli->query("SELECT followed FROM FollowUser WHERE followed_user_id='$user_id' AND following_user_id='$current_user'");
         $num_rows = mysqli_num_rows($follow_results);
         $followed = true;
-        if($num_rows > 0) { // If he has interacted with the post already
+        if($num_rows > 0) { // If he has Interected with the user already
             echo "if 1 echoed";
             $number_of_loops = 0;
             while($row = $follow_results->fetch_assoc()){
@@ -23,7 +23,7 @@
                 header("location: ../views/others_user_account.php?user_id=$user_id");
                 }
             }
-        }else{ // If he hasn't interacted with the post already, create a like object
+        }else{ // If he hasn't interacted with the post already, create a Follow object
             $mysqli->query("INSERT INTO FollowUser (followed_user_id, following_user_id, followed) VALUES ('$user_id', '$current_user', '$followed')") or die($mysqli->error);
             header("location: ../views/others_user_account.php?user_id=$user_id");
         }
---
+++ views/pokes.php
@@ -0,0 +1,105 @@
+<?php  
+require '../config/config.php';
+
+if(!isset($_SESSION['user_logged_in']))   
+    header("location: register.php");
+    $user_id = $_SESSION['user_id']
+?>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Social Media Site</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="../assets/css/home_page_styles.css" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- Responsive navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">SoCiAlSiTe.com!</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="../index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" aria-current="page" href="create_post.php">Post</a>
+                    <li class="nav-item"><a class="nav-link" href="pokes.php">Pokes</a></li>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" aria-current="page"
+                            href="user_account.php">Account</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Page content-->
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <?php if(isset($_SESSION['login_success_message']) || isset($_SESSION['post_sucess']) || isset($_SESSION['post_delete_sucess'])):?>
+            <?php if(isset($_SESSION['login_success_message'])):?>
+            <div class="alert alert-dismissible alert-success text-center" sytle="width:100%">
+                <?php echo $_SESSION['login_success_message']; 
+                                  unset($_SESSION['login_success_message']);
+                            ?>
+            </div>
+            <?php elseif(isset($_SESSION['post_sucess'])): ?>
+            <div class="alert alert-success text-center" sytle="width:100%">
+                <?php echo $_SESSION['post_sucess'];
+                              unset($_SESSION['post_sucess']);
+                        ?>
+            </div>
+            <?php elseif(isset($_SESSION['post_delete_sucess'])): ?>
+            <div class="alert alert-danger text-center" sytle="width:100%">
+                <?php echo $_SESSION['post_delete_sucess'];
+                              unset($_SESSION['post_delete_sucess']);
+                        ?>
+            </div>
+            <?php endif ?>
+            <?php endif ?>
+            <?php if(isset($_SESSION['empty_search_error'])): ?>
+            <div class="alert alert-danger text-center" sytle="width:100%">
+                <?php echo $_SESSION['empty_search_error'];
+                              unset($_SESSION['empty_search_error']);
+                        ?>
+            </div>
+            <?php endif ?>
+            <!-- Blog entries-->
+            <div class="col-lg-8">
+            <?php $mysqli = new mysqli("localhost", "root", "Root*123", "social") or die(mysqli_error($mysqli)); 
+                $result = $mysqli->query("SELECT u.username, up.date_created, u.id FROM UserPoke up
+                                          JOIN User u on up.poker_id = u.id
+                                          WHERE up.pokee_id = $user_id") or die($mysqli->error);
+                ?>
+            <!-- Featured blog post-->
+                <div class="card" >
+                    <ul class="list-group list-group-flush">
+                        <?php while($row = $result->fetch_assoc()):?>
+                        <li class="list-group-item"><div class="fst-italic mb-2"><a style="text-decoration: none;" href="others_user_account.php?user_id=<?php echo $row["id"];?>"><?php echo $row["username"];?></a> poked you on <?php echo $row["date_created"];?></div></li>
+                        <?php endwhile; ?>
+                    </ul>
+                </div>
+            </div>
+            <!-- Side widgets-->
+        </div>
+    </div>
+    <!-- Footer-->
+    <footer class="py-5 bg-dark" style="margin-top:700px;">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Social Media Site 2021</p>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="assets/js/home_page.js"></script>
+</body>
+
+</html>
+    <!-- written by Helen -->
